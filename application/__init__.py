7b22ee232548208796fc3fd7faaf6b128a6fd80b
georgegoldman <georgegoldman2014@gmail.com>
2021-04-25 13:53:17 +0100

    all

--- application/__init__.py
+++ application/__init__.py
@@ -28,6 +28,7 @@ def allowed_file(filename):
     return '.'in filename and \
         filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 bcrypt = Bcrypt(app)
 
 app.config['DEBUG'] = False
